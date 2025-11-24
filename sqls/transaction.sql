cbf6dd97d9ab0fd5ab17135d2e5971e5bbbb8a6a
Maninder Nanda <maninder.nanda@gmail.com>
2025-11-23 18:50:36 -0600

    run snowsql

--- sqls/transaction.sql
+++ sqls/transaction.sql
@@ -1,5 +1,5 @@
 -- Create table TRANSACTION if it doesn't already exist
-CREATE TABLE IF NOT EXISTS TRANSACTION (
+CREATE TABLE IF NOT EXISTS demo_db.demo.TRANSACTION (
     TRANSACTION_ID INT AUTOINCREMENT,       -- Auto-incrementing primary key
     ITEM_ID INT NOT NULL,                   -- Foreign key to ITEM table
     QUANTITY INT NOT NULL,                  -- Quantity of the item
