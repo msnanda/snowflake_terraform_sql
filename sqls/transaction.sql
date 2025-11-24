fbddc6d296b9e80a05bedbf138ad4d1bd4f2ca25
Maninder Nanda <maninder.nanda@gmail.com>
2025-11-23 18:53:26 -0600

    run snowsql

--- sqls/transaction.sql
+++ sqls/transaction.sql
@@ -2,8 +2,6 @@
 CREATE TABLE IF NOT EXISTS demo_db.demo.TRANSACTION (
     TRANSACTION_ID INT AUTOINCREMENT,       -- Auto-incrementing primary key
     ITEM_ID INT NOT NULL,                   -- Foreign key to ITEM table
-    QUANTITY INT NOT NULL,                  -- Quantity of the item
-    TRANSACTION_DATE TIMESTAMP_LTZ DEFAULT CURRENT_TIMESTAMP, -- When the transaction happened
-    PRIMARY KEY (TRANSACTION_ID),
-    FOREIGN KEY (ITEM_ID) REFERENCES ITEM(ITEM_ID)
+    QUANTITY INT NOT NULL                  -- Quantity of the item
+
 );
