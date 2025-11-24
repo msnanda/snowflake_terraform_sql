1dc30e70d6f314ab17575009e9d186ef20c8a4fc
Maninder Nanda <maninder.nanda@gmail.com>
2025-11-23 18:38:38 -0600

    run snowsql

---
+++ sqls/create_item.sql
@@ -0,0 +1,9 @@
+-- Create table ITEM if it doesn't already exist
+CREATE TABLE IF NOT EXISTS ITEM (
+    ITEM_ID INT AUTOINCREMENT,      -- Auto-incrementing primary key
+    ITEM_NAME STRING NOT NULL,      -- Name of the item
+    CATEGORY STRING,                -- Category of the item
+    PRICE NUMBER(10,2),             -- Price with 2 decimals
+    CREATED_AT TIMESTAMP_LTZ DEFAULT CURRENT_TIMESTAMP, -- Creation timestamp
+    PRIMARY KEY (ITEM_ID)
+);
